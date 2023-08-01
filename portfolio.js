1c98e4309ec72d13ceb5cf779f496a13d03f0a1f
saifromhp <79879875+dias-prasad@users.noreply.github.com>
2023-08-01 12:30:55 +0530

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// You can use this JavaScript file to add any interactivity you want to the website.
+// For example, you can use it to handle form submissions or add animations.
+
+// Example of handling the form submission (requires updating the HTML form to have an ID):
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Here you can add code to handle the form submission, like sending the form data to a server or displaying a confirmation message.
+});
+
